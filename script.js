7464106a527178185f445c51a5e4b5789ab52989
NathanStein1 <nathanstein2@gmail.com>
2022-05-11 16:13:16 -0700

    add darkmode

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-// var jumbo = document.querySelector('.jumbotron')
 
-// var zoom = [
-//     {transform: 'min-height'}
-// ]
+let dark = document.querySelector('.dark')
+let navbar = document.querySelector('navbar')
 
+dark.addEventListener('click', function darkness() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    if (navbar.contains('navbar-light', 'bg-light')) {
+        navbar.classList.remove('navbar-light', 'bg-light')
+        navbar.classList.add('navbar-dark', 'bg-dark')
+    }
+  })
+
+
+// $('.switch').click(()=>{
+//     $([".light [class*='-light']", ".dark [class*='-dark']"]).each((i,ele)=>{
+//         $(ele).toggleClass('bg-light bg-dark')
+//         $(ele).toggleClass('text-light text-dark')
+//         $(ele).toggleClass('navbar-light navbar-dark')
+//     })
+//     // toggle body class selector
+//     $('body').toggleClass('light dark')
+// })
